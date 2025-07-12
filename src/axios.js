f70ece77fac079d598ac72391ce7b9d707569174
Damilare <da9783790@gmil.com>
2025-07-12 17:54:05 +0100

    Update the axios.js file commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://task-master-sooty.vercel.app',
+  baseURL: 'https://task-master-backend-nu.vercel.app',
   withCredentials: true,
 });
 
